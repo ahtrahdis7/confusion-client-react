31159bf19f5ea1d3d082b447e83e26885900c1ad
ahtrahdis7 <sidharthabiki@gmail.com>
2020-06-21 11:59:03 +0530

    combining reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal, ModalHeader,
+     ModalBody, Label, Col, } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
 import { Link } from 'react-router-dom';
  
     function RenderDish({dish}){
@@ -19,6 +22,7 @@ import { Link } from 'react-router-dom';
                 <div></div>
             );
     }
+
     function RenderComments({comments}) {
         const cmmt = comments.map((comment) => {
             return (
@@ -33,43 +37,135 @@ import { Link } from 'react-router-dom';
             );
         });
         return (
-            <Card>
-                <h4>Comments</h4>
-                <CardText>{cmmt}</CardText>
-            </Card>
+                <div>
+                    <h4>Comments</h4>
+                    <p>
+                        {cmmt}
+                    </p>
+                </div>
+
         );
     }
 
-    const DishDetail = (props) => {
-        console.log('DishDetail component render Invoked')
-        if(props.dish != null){
-            return (
-                <div className="container">
+
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
+    const maxValue = (len) => (val) => !(val) || (val <= len);
+    const minValue = (len) => (val) => val && (val >= len);
+
+    class DishDetail extends Component{
+        constructor(props){
+            super(props);
+
+            this.state = {
+                isModalOpen: false
+            };
+            this.toggleModal = this.toggleModal.bind(this);
+            this.handleLogin = this.handleSubmit.bind(this);
+        }
+
+        toggleModal() {
+            this.setState({
+              isModalOpen: !this.state.isModalOpen
+            });
+          }
+
+
+        handleSubmit(values) {
+            this.toggleModal();
+            console.log('Current State is: ' + JSON.stringify(values));
+            alert('Current State is: ' + JSON.stringify(values));
+        }
+
+        render() {
+            if(this.props.dish != null){
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className="col-12">
+                                <h3>{this.props.dish.name}</h3>
+                                <hr />
+                            </div>                
+                        </div>
                     <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>                
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={this.props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={this.props.comments} /><br></br>
+                            <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+
+                            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                                <ModalHeader toggle={this.toggleModal}>Comment</ModalHeader>
+                                <ModalBody>
+                                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                                                <Label htmlFor="rating" md={2}>Rating</Label>
+                                                <Col md={12}>
+                                                    <Control.select
+                                                        model=".rating"
+                                                        id=".rating"
+                                                        name=".rating"
+                                                        className="form-control"
+                                                        >
+                                                        <option value="1">1</option>
+                                                        <option value="2">2</option>
+                                                        <option value="3">3</option>
+                                                        <option value="4">4</option>
+                                                        <option value="5">5</option>
+                                                    </Control.select>
+                                                </Col>
+                                                <Label htmlFor="username" md={2}>Username</Label>
+                                                <Col md={12}>
+                                                    <Control.text model=".username" id="username" name="username"
+                                                        placeholder="Author"
+                                                        className="form-control"
+                                                        validators={{
+                                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                                        }}
+                                                        />
+                                                    <Errors
+                                                        className="text-danger"
+                                                        model=".username"
+                                                        show="touched"
+                                                        messages={{
+                                                            required: 'Required',
+                                                            minLength: 'Must be greater than 2 characters',
+                                                            maxLength: 'Must be 15 characters or less'
+                                                        }}
+                                                    />
+                                                </Col>
+                                                <Label htmlFor="message" md={2}>Comment</Label>
+                                                <Col md={12}>
+                                                    <Control.textarea model=".message" id="message" name="message"
+                                                        rows="6"
+                                                        placeholder="Your Comment goes Here"
+                                                        className="form-control" />
+                                                </Col>
+                                            {/* <Button type="submit" value="submit" color="primary">Add</Button> */}
+                                            <br></br>
+                                            <Col md={{size:10, offset: 2}}>
+                                                <Button type="submit" color="primary">
+                                                Add Comment
+                                                </Button>
+                                            </Col>
+                                    </LocalForm>
+                                </ModalBody>
+                            </Modal>
+                        </div>
                     </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                );
+            } else {
+                return (
+                    <div>
                     </div>
-                </div>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                </div>
-            );
+                );
+            }
         }
     }
 export default DishDetail;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -77,30 +77,30 @@ class Header extends Component {
                   </div>
               </Jumbotron>
               <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.handleLogin}>
-                              <FormGroup>
-                                  <Label htmlFor="username">Username</Label>
-                                  <Input type="text" id="username" name="username"
-                                      innerRef={(input) => this.username = input} />
-                              </FormGroup>
-                              <FormGroup>
-                                  <Label htmlFor="password">Password</Label>
-                                  <Input type="password" id="password" name="password"
-                                      innerRef={(input) => this.password = input}  />
-                              </FormGroup>
-                              <FormGroup check>
-                                  <Label check>
-                                      <Input type="checkbox" name="remember"
-                                      innerRef={(input) => this.remember = input}  />
-                                      Remember me
-                                  </Label>
-                              </FormGroup>
-                              <Button type="submit" value="submit" color="primary">Login</Button>
-                        </Form>
-                    </ModalBody>
-                </Modal>
+                  <ModalHeader toggle={this.toggleModal}>LOGIN</ModalHeader>
+                  <ModalBody>
+                      <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                      </Form>
+                  </ModalBody>
+              </Modal>
           </React.Fragment>
       );
     }
